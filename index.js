0bb661bbed0b775888d010c4ce25f6064a98c7ea
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-08 15:18:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function Driver(name, age, experience) {
   this.experience = experience
 }
 
-function Driver(name, age, experience) {
+function PickupLocation(name, age, experience) {
   this.name = name
   this.age = age
   this.experience = experience
